ca171a03a8a72fa2d6d2c8f238b8205e3d8fbaa2
WONG ZHAO XUAN <112739121+wzhaoxuan@users.noreply.github.com>
2024-09-03 12:09:11 +0800

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,9 +1,8 @@
-//How the dice works
-
+package Boat_Race;
 import java.util.Random;
 
 public class Dice {
-
+	
 	public int roll() {
 		Random random = new Random();
 		return random.nextInt(6) + 1;
